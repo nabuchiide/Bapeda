9b5eac41ef138dccca8aa4bc2561655ee3d5dc7d
Hendi <hendi@eluon.com>
2021-11-28 11:40:09 +0700

    update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,11 @@
 <?php 
 
 define('BASEURL','http://localhost/bappeda/public');
-define('APL_NAME','bapenda');
+define('APL_NAME','bappeda');
 //DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME','bapendadb');
+define('DB_NAME','bappedadb');
 
 
