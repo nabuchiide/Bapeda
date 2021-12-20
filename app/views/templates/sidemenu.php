db38c4207de243c5d13e993c4ef76122e4c923bc
Hendi <hendi@eluon.com>
2021-12-20 15:38:04 +0700

    Update sidemenu.php

--- app/views/templates/sidemenu.php
+++ app/views/templates/sidemenu.php
@@ -126,6 +126,8 @@
                                 <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-bullseye"></i> <span> Kegiatan </span> <span class="float-right"><i class="mdi mdi-chevron-right"></i></span></a>
                                 <ul class="list-unstyled">
                                     <li><a href="<?= BASEURL ?>/pajak">Pajak</a></li>
+                                    <li><a href="<?= BASEURL ?>/anggaran">Anggaran</a></li>
+
                                 </ul>
                             </li>
 
